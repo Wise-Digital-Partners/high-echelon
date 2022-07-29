608710ff83d7e409da7d5afe45189edc6076b8c1
Senior Full Stack & Blockchain Developer <94433423+WebMaster0405@users.noreply.github.com>
2022-07-29 12:06:32 -0500

    Update gatsby-config.js

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -9,7 +9,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const {
   NODE_ENV,
-  URL: NETLIFY_SITE_URL = "",
+  URL: NETLIFY_SITE_URL = "https://high-echelon.netlify.app",
   DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
   CONTEXT: NETLIFY_ENV = NODE_ENV,
 } = process.env;
