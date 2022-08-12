8351f693eb0d1ab87ecd617fd23eab8e372eff72
Zac Wood <57919069+Zac-Wood@users.noreply.github.com>
2022-08-12 13:11:10 -0700

    Update privacy-policy.js

--- web/src/pages/privacy-policy.js
+++ web/src/pages/privacy-policy.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Layout from "../components/Layout";
-import ButtonSolid from "../Button/ButtonSolid";
+import ButtonSolid from "../components/Button/ButtonSolid";
 
 const Page = () => {
   return (
